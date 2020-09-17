8371862af0c37876ba09e15cd60fca5479c33c5b
MuhammadZulfikarIkhsan <71245150+MuhammadZulfikarIkhsan@users.noreply.github.com>
2020-09-17 14:18:25 +0700

    HelloWorld.java

---
+++ Jurnal02/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+  public static void main(String[] args){
+    System.out.println("Nama : Muhammad Zulfikar Ikhsan");
+    System.out.println("NIM : D3IF-6706202049");
+    System.out.println("Kelas : 44-03");
+  }
+}
